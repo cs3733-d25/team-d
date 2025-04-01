b153b2aa79398ff1a327bba1d70cb03fa1e61853
christinenngo <christinengo05@gmail.com>
2025-04-01 19:13:50 -0400

    service requests post 2

--- apps/backend/src/routes/servicereqs.ts
+++ apps/backend/src/routes/servicereqs.ts
@@ -7,13 +7,20 @@ const router: Router = express.Router();
 // TODO: handle a GET request
 // Use router.get for a GET request, router.post for a POST request, etc.
 // Refer to score.ts.
+router.get('/', async function (req: Request, res: Response) {
+    const requests = await PrismaClient.translatorRequest.findMany();
+    console.log(requests);
+    // res.send(requests);
+    // res.status(200).send(requests);
+    res.json(requests);
+});
 
 // handles a POST request
 router.post('/', async function (req: Request, res: Response) {
     const serviceReqAttempt: Prisma.TranslatorRequestCreateInput = req.body;
 
     try {
-        await PrismaClient.servicereqs.create({ data: serviceReqAttempt });
+        await PrismaClient.translatorRequest.create({ data: serviceReqAttempt });
         console.info('Successfully created service request'); // log success
     } catch (error) {
         console.error(`Unable to submit service request ${serviceReqAttempt}: ${error}`);
