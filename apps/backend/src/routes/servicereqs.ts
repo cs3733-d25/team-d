7fa0d765b567de2b6845cfc71c89bb240eb9f9bf
christinenngo <christinengo05@gmail.com>
2025-04-01 18:31:56 -0400

    service requests post

--- apps/backend/src/routes/servicereqs.ts
+++ apps/backend/src/routes/servicereqs.ts
@@ -8,6 +8,22 @@ const router: Router = express.Router();
 // Use router.get for a GET request, router.post for a POST request, etc.
 // Refer to score.ts.
 
+// handles a POST request
+router.post('/', async function (req: Request, res: Response) {
+    const serviceReqAttempt: Prisma.TranslatorRequestCreateInput = req.body;
+
+    try {
+        await PrismaClient.servicereqs.create({ data: serviceReqAttempt });
+        console.info('Successfully created service request'); // log success
+    } catch (error) {
+        console.error(`Unable to submit service request ${serviceReqAttempt}: ${error}`);
+        res.sendStatus(400); // send error
+        return;
+    }
+
+    res.sendStatus(200);
+});
+
 // IMPORTANT!! Make sure the path is defined in app.ts!
 // I put a to-do on where to do this.
 
