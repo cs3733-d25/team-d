4a4e07bd7082b65cd4372204cfb18b2c66f227f3
jabo23 <108074850+jabo23@users.noreply.github.com>
2025-04-01 22:03:32 -0400

    Revert "Messed around with the example page styling for practice"

--- apps/frontend/src/routes/ExamplePage.tsx
+++ apps/frontend/src/routes/ExamplePage.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ExampleComponent from '../components/ExampleComponent.tsx';
-import '../styles/styles.css';
 
 const ExamplePage = () => {
     return (
         <div className="p-10">
             <h1 className="font-bold text-xl pb-4">Example Page</h1>
-            <p>Psst, did you know it's April Fools Today????</p>
-            <p>NO?!</p>
             <ExampleComponent></ExampleComponent>
         </div>
     );
