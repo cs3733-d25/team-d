40e8182e41d842eb36c982c1ac3544def8123af3
avoforme <hophuongthanh028@gmail.com>
2025-04-02 18:03:54 -0400

    fix router: router working

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,47 +1,39 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// This is how we import pages:
-import Login from './routes/Login.tsx';
-import Map from './routes/Map.tsx';
-import Directory from './routes/Directory.tsx';
-import ServiceRequest from './routes/ServiceRequest.tsx';
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// // This is how we import pages:
+// import Login from './routes/Login.tsx';
+// import Map from './routes/Map.tsx';
+// import Directory from './routes/Directory.tsx';
+// import ServiceRequest from './routes/ServiceRequest.tsx';
 
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            errorElement: <div />,
-            element: <Login />,
-        },
-        // The below is an example
-        {
-            path: '/map',
-            errorElement: <div />,
-            element: <Map />,
-        },
-        {
-            path: '/directory',
-            errorElement: <div />,
-            element: <Directory />,
-        },
-        {
-            path: '/servicerequest',
-            errorElement: <div />,
-            element: <ServiceRequest />,
-        },
-    ]);
+    // const router = createBrowserRouter([
+    //     {
+    //         path: '/',
+    //         errorElement: <div />,
+    //         element: <Login />,
+    //     },
+    //     // The below is an example
+    //     {
+    //         path: '/map',
+    //         errorElement: <div />,
+    //         element: <Map />,
+    //     },
+    //     {
+    //         path: '/directory',
+    //         errorElement: <div />,
+    //         element: <Directory />,
+    //     },
+    //     {
+    //         path: '/servicerequest',
+    //         errorElement: <div />,
+    //         element: <ServiceRequest />,
+    //     },
+    // ]);
     function App() {
     return (
         <>
-            <nav>
-                <a href="/">Login</a>
-                <a href="/map">Map</a>
-                <a href="/directory">Directory</a>
-                <a href="/servicerequest">Service Request</a>
-            </nav>
+            <h1>hi </h1>
 
-
-            {/*Pages appear here*/}
-            <RouterProvider router={router} />
         </>
     );
 }
