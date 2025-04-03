ffd9b82496e0a856571d79b62ece8255ba8ea67b
BramRBN2004 <smallb869@gmail.com>
2025-04-03 19:01:18 -0400

    started home page

--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {Button} from "@/components/ui/button.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Label} from "@/components/ui/label.tsx";
+
+type HomeProps = {
+    buildingDirectory: string;
+}
 
 export default function Home() {
     return (
         <>
-            <h1>Home Page here!</h1>
+            <div className="bg-[url(/src/public/Hospital.jpg)]..."></div>
+            <Card m-header="16px">
+                <CardTitle className="text-[#000000]"></CardTitle>
+                <h1 className="text-6xl text-black">Building Directory</h1>
+            </Card>
+            <CardContent>
+
+            </CardContent>
+
         </>
     );
 }
