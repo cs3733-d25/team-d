8969bd65e520d2cbb956aa2b2112fa60d7c3d9a1
jabo23 <jacob8086b@gmail.com>
2025-04-03 10:25:58 -0400

    Created a prototype form for service requests

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,56 +4,39 @@ import {useForm} from "react-hook-form";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {Input} from "@/components/ui/input.tsx";
+
+type fieldItem = {
+    name: string;
+    field: 'languageFrom' | 'languageTo' | 'roomNumber' | 'startDateTime' | 'endDateTime';
+};
+
+const fields: fieldItem[] = [
+    {
+        field: 'languageFrom',
+        name: 'Language From',
+    },
+    {
+        field: 'languageTo',
+        name: 'Language To',
+    },
+    {
+        field: 'roomNumber',
+        name: 'Room Number',
+    },
+    {
+        field: 'startDateTime',
+        name: 'Start',
+    },
+    {
+        field: 'endDateTime',
+        name: 'End',
+    },
+];
 
 function App() {
-
-
-    const formSchema = z.object({
-        languageFrom: z.string(),
-        languageTo: z.string(),
-        roomNumber: z.string(),
-        startDateTime: z.string(),
-        endDateTime: z.string(),
-    });
-
-
-
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            languageFrom: '',
-            languageTo: '',
-            roomNumber: '',
-            startDateTime: '',
-            endDateTime: '',
-        },
-    });
-
-    const onSubmit = (data: z.infer<typeof formSchema>) => {
-        console.log(data);
-    }
-
     return (
-        <>
-            <div className={"background flex"}>
-                <Form {...form} >
-                    <form onSubmit={form.handleSubmit(onSubmit)}>
-                        <FormField
-                            control={form.control}
-                            name="languageFrom"
-                            render={(field) => (
-                                <FormItem>
-                                    <FormLabel>Language From</FormLabel>
-                                    <FormControl>
-
-                                    </FormControl>
-                                </FormItem>
-                            )}/>
-                        <Button type="submit">Submit</Button>
-                    </form>
-                </Form>
-            </div>
-        </>
+        <h1>Home Page</h1>
     );
 }
 
--- apps/frontend/src/routes/ServiceRequest.tsx
+++ apps/frontend/src/routes/ServiceRequest.tsx
@@ -1,16 +1,122 @@
-// <replace with imports>
 import React from 'react';
+import z from 'zod';
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form.tsx";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {Button} from "@/components/ui/button.tsx";
+import {Input} from "@/components/ui/input.tsx";
+
+type fieldItem = {
+    name: string;
+    field: 'languageFrom' | 'languageTo' | 'roomNumber' | 'startDateTime' | 'endDateTime';
+};
+
+const fields: fieldItem[] = [
+    {
+        field: 'languageFrom',
+        name: 'Language From',
+    },
+    {
+        field: 'languageTo',
+        name: 'Language To',
+    },
+    {
+        field: 'roomNumber',
+        name: 'Room Number',
+    },
+    {
+        field: 'startDateTime',
+        name: 'Start',
+    },
+    {
+        field: 'endDateTime',
+        name: 'End',
+    },
+];
 
 export default function ServiceRequest() {
+    const formSchema = z.object({
+        languageFrom: z.string(),
+        languageTo: z.string(),
+        roomNumber: z.string(),
+        startDateTime: z.string(),
+        endDateTime: z.string(),
+    });
 
 
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            languageFrom: '',
+            languageTo: '',
+            roomNumber: '',
+            startDateTime: '',
+            endDateTime: '',
+        },
+    });
+
+    const onSubmit = (data: z.infer<typeof formSchema>) => {
+        console.log(data);
+    };
 
     return (
-        <div id={"service-request"}>
-            {/*TODO: replace <p> with actual page*/}
-            <p>ServiceRequest</p>
-        </div>
+        <>
+            <>
+                <Card className={"w-[400px] place-content-center"}>
+                    <CardHeader>
+                        <CardTitle>Request a Translator</CardTitle>
+                    </CardHeader>
+                    <CardContent >
+                        <Form {...form} >
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                                {fields.map((item) => (
+                                    <FormField
+                                        control={form.control}
+                                        name={item.field}
+                                        key={item.field}
+                                        render={({field}) => (
+                                            <FormItem>
+                                                <FormLabel>{item.name}</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder={""} {...field} />
+                                                </FormControl>
+                                            </FormItem>
 
+                                        )}/>
+                                ))}
+                                <Button type="submit">Submit</Button>
+                            </form>
+                        </Form>
+                    </CardContent>
+                </Card>
+
+            </>
+        </>
     );
+
+    // return (
+    //     <div id={"service-request"}>
+    //         {/*TODO: replace <p> with actual page*/}
+    //         <p>ServiceRequest</p>
+    //     </div>
+    //
+    // );
 }
 
