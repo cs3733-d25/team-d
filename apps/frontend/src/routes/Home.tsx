a51c093fcf07366fa8d076d6584f9ef9208a13f3
BramRBN2004 <smallb869@gmail.com>
2025-04-03 20:13:40 -0400

    finished prototype home page

--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -1,30 +1,40 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import {
     Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import {Button} from "@/components/ui/button.tsx";
-import {Input} from "@/components/ui/input.tsx";
-import {Label} from "@/components/ui/label.tsx";
 
-type HomeProps = {
-    buildingDirectory: string;
-}
+
 
 export default function Home() {
     return (
         <>
-            <div className="bg-[url(/src/public/Hospital.jpg)]..."></div>
-            <Card m-header="16px">
-                <CardTitle className="text-[#000000]"></CardTitle>
-                <h1 className="text-6xl text-black">Building Directory</h1>
-            </Card>
-            <CardContent>
+            <div className="bg-[url(../public/Hospital.jpg)] bg-no-repeat bg-cover h-screen p-20 ">
+                <Card className="bg-blue-700/70 m-20 ">
+                    <CardTitle className="text-6xl text-center text-white ">Welcome!</CardTitle>
+                </Card>
 
-            </CardContent>
+                <div className="justify-center flex flex-row">
+                    <div className="w-1/3 p-20">
+                        <Link to={`/login`} className="text-3xl text-center">
+                            <Card className="bg-blue-700/70 hover:bg-blue-700 justify-center h-50 text-white ">Login</Card>
+                        </Link>
+                    </div>
+
+                    <div className="w-1/3 p-20">
+                        <Link to={`/map`} className="text-3xl text-center">
+                            <Card className="bg-blue-700/70 hover:bg-blue-700 justify-center h-50 text-white ">Visitor Map</Card>
+                        </Link>
+                    </div>
+
+                    <div className="w-1/3 p-20">
+                        <Link to={`/directory`} className="text-3xl text-center">
+                            <Card className="bg-blue-700/70 hover:bg-blue-700 justify-center h-50 text-white ">Directory</Card>
+                        </Link>
+                    </div>
+                </div>
+            </div>
 
         </>
     );
