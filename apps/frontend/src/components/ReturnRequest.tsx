29cdf03f4749ba2293f81c62ed48b6a1934ee70c
jabo23 <jacob8086b@gmail.com>
2025-04-03 21:19:46 -0400

    Made a request summary on request submission

--- apps/frontend/src/components/ReturnRequest.tsx
+++ apps/frontend/src/components/ReturnRequest.tsx
@@ -1,31 +1,33 @@
-import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import {Label} from "@/components/ui/label.tsx";
-import {Input} from "@/components/ui/input.tsx";
-import {Button} from "@/components/ui/button.tsx";
+type translatorRequestForm = {
+    languageFrom: string;
+    languageTo: string;
+    roomNumber: string;
+    startDateTime: string;
+    endDateTime: string;
+}
 
-const ReturnRequest = () => {
+const ReturnRequest = (props: translatorRequestForm) => {
 
     return (
         <div className="grid place-items-center h-full items-center">
             <Card className="w-1/3">
                 <CardHeader className="place-content-center">
-                    <CardTitle className="text-3xl">Request a Translator</CardTitle>
+                    <CardTitle className="text-3xl">Request Summary</CardTitle>
                 </CardHeader>
 
                     <CardContent>
                         <Label htmlFor="languageFrom">Language From</Label>
-                            <p>Spanish</p>
+                            <p>{props.languageFrom}</p>
                         <Label htmlFor="languageTo">Language To</Label>
-                            <p>English</p>
+                            <p>{props.languageTo}</p>
                         <Label htmlFor="roomNumber">Room Number</Label>
-                            <p>405</p>
+                            <p>{props.roomNumber}</p>
                         <Label htmlFor="startDateTime">Start Date and Time</Label>
-                            <p>06/25/2004</p>
+                            <p>{props.startDateTime}</p>
                         <Label htmlFor="endDateTime">End Date and Time</Label>
-                            <p>06/25/2004</p>
-                        <br />
-                        <p>Is this information correct?</p>
-                        <button>Yes</button> <button>No</button>
+                            <p>{props.endDateTime}</p>
 
                     </CardContent>
             </Card>
--- apps/frontend/src/routes/ServiceRequest.tsx
+++ apps/frontend/src/routes/ServiceRequest.tsx
@@ -10,6 +10,7 @@ import {Button} from "@/components/ui/button.tsx";
 import {Input} from "@/components/ui/input.tsx";
 import {Label} from "@/components/ui/label.tsx";
 import RequestStatus from "@/components/RequestStatus.tsx";
+import ReturnRequest from "@/components/ReturnRequest.tsx";
 import {useState} from "react";
 
 type translatorRequestForm = {
@@ -30,9 +31,12 @@ export default function ServiceRequest() {
         endDateTime: '',
     });
 
+    const [submitted, setSubmitted] = React.useState(false);
+
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log(form);
+        setSubmitted(true);
     }
 
     const [showMessage, setShowMessage] = useState(false);
@@ -42,80 +46,86 @@ export default function ServiceRequest() {
     }
 
     return (
-        <div className="grid place-items-center h-full items-center">
-            <Card className="w-1/3">
-                <CardHeader className="place-content-center">
-                    <CardTitle className="text-3xl">Request a Translator</CardTitle>
-                </CardHeader>
-                <form onSubmit={onSubmit}>
-                    <CardContent>
-                        <Label htmlFor="languageFrom">Language From</Label>
-                        <Input
-                            type="text"
-                            id="languageFrom"
-                            onChange={(e) =>
-                                setForm({
-                                    ...form,
-                                    languageFrom: e.target.value,
-                                })
-                            }
-                        />
-                        <Label htmlFor="languageTo">Language To</Label>
-                        <Input
-                            type="text"
-                            id="languageTo"
-                            onChange={(e) =>
-                                setForm({
-                                    ...form,
-                                    languageTo: e.target.value,
-                                })
-                            }
-                        />
-                        <Label htmlFor="roomNumber">Room Number</Label>
-                        <Input
-                            type="text"
-                            id="roomNumber"
-                            onChange={(e) =>
-                                setForm({
-                                    ...form,
-                                    roomNumber: e.target.value,
-                                })
-                            }
-                        />
-                        <Label htmlFor="startDateTime">Start Date and Time</Label>
-                        <Input
-                            type="datetime-local"
-                            id="startDateTime"
-                            onChange={(e) =>
-                                setForm({
-                                    ...form,
-                                    startDateTime: e.target.value,
-                                })
-                            }
-                        />
-                        <Label htmlFor="endDateTime">End Date and Time</Label>
-                        <Input
-                            type="datetime-local"
-                            id="languageFrom"
-                            onChange={(e) =>
-                                setForm({
-                                    ...form,
-                                    endDateTime: e.target.value,
-                                })
-                            }
-                        />
-                    </CardContent>
-                    <CardFooter>
-                        {!showMessage && (
-                            <Button onClick={whenClicked} type="submit">
-                                Submit
-                            </Button>
-                        )}
-                        {showMessage && <RequestStatus status="message" />}
-                    </CardFooter>
-                </form>
-            </Card>
-        </div>
+        <>
+            {!submitted ?
+                <div className="grid place-items-center h-full items-center">
+                    <Card className="w-1/3">
+                        <CardHeader className="place-content-center">
+                            <CardTitle className="text-3xl">Request a Translator</CardTitle>
+                        </CardHeader>
+                        <form onSubmit={onSubmit}>
+                            <CardContent>
+                                <Label htmlFor="languageFrom">Language From</Label>
+                                <Input
+                                    type="text"
+                                    id="languageFrom"
+                                    onChange={(e) =>
+                                        setForm({
+                                            ...form,
+                                            languageFrom: e.target.value,
+                                        })
+                                    }
+                                />
+                                <Label htmlFor="languageTo">Language To</Label>
+                                <Input
+                                    type="text"
+                                    id="languageTo"
+                                    onChange={(e) =>
+                                        setForm({
+                                            ...form,
+                                            languageTo: e.target.value,
+                                        })
+                                    }
+                                />
+                                <Label htmlFor="roomNumber">Room Number</Label>
+                                <Input
+                                    type="text"
+                                    id="roomNumber"
+                                    onChange={(e) =>
+                                        setForm({
+                                            ...form,
+                                            roomNumber: e.target.value,
+                                        })
+                                    }
+                                />
+                                <Label htmlFor="startDateTime">Start Date and Time</Label>
+                                <Input
+                                    type="datetime-local"
+                                    id="startDateTime"
+                                    onChange={(e) =>
+                                        setForm({
+                                            ...form,
+                                            startDateTime: e.target.value,
+                                        })
+                                    }
+                                />
+                                <Label htmlFor="endDateTime">End Date and Time</Label>
+                                <Input
+                                    type="datetime-local"
+                                    id="languageFrom"
+                                    onChange={(e) =>
+                                        setForm({
+                                            ...form,
+                                            endDateTime: e.target.value,
+                                        })
+                                    }
+                                />
+                            </CardContent>
+                            <CardFooter>
+                                <Button type="submit">Submit</Button>
+                            </CardFooter>
+                        </form>
+                    </Card>
+                </div>
+            :
+                <ReturnRequest
+                    languageFrom={form.languageFrom}
+                    languageTo={form.languageTo}
+                    roomNumber={form.roomNumber}
+                    startDateTime={form.startDateTime}
+                    endDateTime={form.endDateTime}/>
+            }
+        </>
     );
 }
 
