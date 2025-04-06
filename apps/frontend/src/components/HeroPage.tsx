149ee3e53badbabdef01b755be778ad3cd7976e2
BramRBN2004 <smallb869@gmail.com>
2025-04-06 16:19:32 -0400

    Integrated Hero message into home page

--- apps/frontend/src/components/HeroPage.tsx
+++ apps/frontend/src/components/HeroPage.tsx
@@ -5,7 +5,7 @@ import {Card} from "@/components/ui/card.tsx";
 export default function HeroTextBox(){
     return(
         <>
-            <div className="w-2/3">
+            <div className="w-2/3 animate-in fade-in zoom-in duration-800">
                 <Card  className="pl-3 pr-75 pb-25 pt-25 bg-cyan-200/80 text-5xl/17 box-border rounded-none border-0 ">
                     Navigating to Your Appointment Just Got Easier.</Card>
             </div>
--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -6,21 +6,21 @@ import {
 } from "@/components/ui/card"
 
 import Login from "@/components/Login.tsx";
+import HeroTextBox from "@/components/HeroPage.tsx";
 
 
 export default function Home() {
     return (
         <>
             <div className="object-left bg-[url(../public/Hospital.jpg)] bg-no-repeat bg-cover h-screen p-10 overflow-y-hidden flex flex-row ">
-                {/*<Card className="bg-blue-700/70 m-20 ">*/}
-                {/*    <CardTitle className="text-6xl text-center text-white ">Welcome!</CardTitle>*/}
-                {/*</Card>*/}
-                <div className="flex-2">
-                    {/*Hero message here*/}
+
+                {/*This is the call of the component HeroTextBox for the home page*/}
+                <div className="flex-2/3 place-content-center">
+                    <HeroTextBox />
                 </div>
 
                 {/*This is the call of the component login for the home page*/}
-                <div className="flex-1 place-content-center">
+                <div className="w-200 place-content-center">
                     <Login />
                 </div>
             </div>
