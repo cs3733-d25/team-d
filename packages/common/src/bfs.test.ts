e50a60427b1b7549278573823609588ae1a2a680
Jiaming Du (Felix) <felixdu10>
2025-04-07 18:30:31 -0400

    update test with coord

--- packages/common/src/bfs.test.ts
+++ packages/common/src/bfs.test.ts
@@ -2,20 +2,20 @@ import { describe, test, expect } from 'vitest';
 import { Graph } from './bfs';
 import type { Coordinates } from './constants';
 
-const dummyCoord: Coordinates = { x: 0, y: 0 };
+test('Graph BFS > BFS finds correct path between nodes', () => {
+    const graph = new Graph();
 
-describe('Graph BFS', () => {
-    test('BFS finds correct path between nodes', () => {
-        const graph = new Graph();
+    const A: Coordinates = { x: 0, y: 0 };
+    const B: Coordinates = { x: 1, y: 0 };
+    const C: Coordinates = { x: 2, y: 0 };
 
-        graph.addNode('A', dummyCoord);
-        graph.addNode('B', dummyCoord);
-        graph.addNode('C', dummyCoord);
+    graph.addNode('A', A);
+    graph.addNode('B', B);
+    graph.addNode('C', C);
 
-        graph.addEdge('A', 'B');
-        graph.addEdge('B', 'C');
+    graph.addEdge('A', 'B');
+    graph.addEdge('B', 'C');
 
-        const path = graph.bfs('A', 'C');
-        expect(path).toEqual(['A', 'B', 'C']);
-    });
+    const path = graph.bfs('A', 'C');
+    expect(path).toEqual([A, B, C]);
 });
