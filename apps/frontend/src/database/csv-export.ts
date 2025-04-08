1cb853e07089aaa3bb6ce8a43e00025779ff8cd7
himynameismaggie <mghosie@wpi.edu>
2025-04-07 20:45:30 -0400

    fixed all csv export red squiggly lines

--- apps/frontend/src/database/csv-export.ts
+++ apps/frontend/src/database/csv-export.ts
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
+interface Department{
+    departmentId: number;
+    name: string;
+    floor: number;
+    suite: string;
+    specialtyServices: string;
+    hours: string;
+    telephone: string;
+}
+
 
 export async function GetDirectory() {
     const data = (await axios.get('/api/department')).data;
     const cols = Object.keys(data[0]);
     const colsString = cols.join(',');
-    const employees = data.map((row) => cols.map((fieldName) =>
-        JSON.stringify(row[fieldName])).join(',')
+    const departments = data.map((row:Department) => cols.map((fieldName) =>
+        JSON.stringify(row[fieldName as keyof Department])).join(',')
     );
     //join cols and body, and break into separate lines
-    const csv = [colsString, ...employees].join('\r\n');
+    const csv = [colsString, ...departments].join('\r\n');
     const blob = new Blob([csv], { type: "text/csv;charset=UTF-8" });
     const link = document.createElement("a");
     link.href = window.URL.createObjectURL(blob);
