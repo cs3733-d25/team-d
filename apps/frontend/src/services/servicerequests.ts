eb0689d5933cf62557ad8267dcab65e84c4a598e
christinenngo <christinengo05@gmail.com>
2025-04-07 23:56:50 -0400

    made SubmitTranslatorRequest (post) function for frontend to use

--- apps/frontend/src/services/servicerequests.ts
+++ apps/frontend/src/services/servicerequests.ts
@@ -2,9 +2,21 @@ import axios from 'axios';
 
 import { SERVICEREQS } from 'packages/common/src/constants.ts'
 
-const requests = () => {
-    const services = axios.get(SERVICEREQS);
-    return services.data;
+// const requests = () => {
+//     const services = axios.get(SERVICEREQS);
+//     return services.data;
+// }
+
+export interface TranslatorRequestForm {
+    languageFrom: string;
+    languageTo: string;
+    roomNum: string;
+    startDateTime: Date;
+    endDateTime: Date;
 }
 
-export default requests;
+export async function SubmitTranslatorRequest(request: TranslatorRequestForm) {
+    await axios.post('/api/servicereqs', request);
+}
+
+// export default requests;
