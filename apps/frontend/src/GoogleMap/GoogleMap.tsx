a61d0a37f62897c6c71f123c1c08fbc7a4b1e1fa
avoforme <hophuongthanh028@gmail.com>
2025-04-10 23:46:49 -0400

    overlay works but doesnt align with ggl map well

--- apps/frontend/src/GoogleMap/GoogleMap.tsx
+++ apps/frontend/src/GoogleMap/GoogleMap.tsx
@@ -5,6 +5,19 @@ const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 import AutocompleteDirectionsHandler from "@/GoogleMap/GoogleMapHelper.ts";
 // import USGSOverlay from "@/GoogleMap/GoogleMapOverlay.ts";
 
+import floor_1_map from "@/public/floor1transparent.png";
+
+// Declare window type extension
+declare global {
+    interface Window {
+        initMap: () => void;
+        google: typeof google;
+    }
+}
+
+
+let historicalOverlay;
+
 const GGMap: React.FC = () => {
     const mapRef = useRef<HTMLDivElement | null>(null);
 
@@ -30,129 +43,13 @@ const GGMap: React.FC = () => {
         // Attach initMap to window for Google callback
         window.initMap = () => {
 
-            class USGSOverlay extends google.maps.OverlayView {
-                private bounds: google.maps.LatLngBounds;
-                private image: string;
-                private div?: HTMLElement;
-
-                constructor(bounds: google.maps.LatLngBounds, image: string) {
-                    super();
-
-                    this.bounds = bounds;
-                    this.image = image;
-                }
-
-                /**
-                 * onAdd is called when the map's panes are ready and the overlay has been
-                 * added to the map.
-                 */
-                onAdd() {
-                    this.div = document.createElement("div");
-                    this.div.style.borderStyle = "none";
-                    this.div.style.borderWidth = "0px";
-                    this.div.style.position = "absolute";
-
-                    // Create the img element and attach it to the div.
-                    const img = document.createElement("img");
-
-                    img.src = this.image;
-                    img.style.width = "100%";
-                    img.style.height = "100%";
-                    img.style.position = "absolute";
-                    this.div.appendChild(img);
-
-                    // Add the element to the "overlayLayer" pane.
-                    const panes = this.getPanes()!;
-
-                    panes.overlayLayer.appendChild(this.div);
-                }
-
-                draw() {
-                    // We use the south-west and north-east
-                    // coordinates of the overlay to peg it to the correct position and size.
-                    // To do this, we need to retrieve the projection from the overlay.
-                    const overlayProjection = this.getProjection();
-
-                    // Retrieve the south-west and north-east coordinates of this overlay
-                    // in LatLngs and convert them to pixel coordinates.
-                    // We'll use these coordinates to resize the div.
-                    const sw = overlayProjection.fromLatLngToDivPixel(
-                        this.bounds.getSouthWest()
-                    )!;
-                    const ne = overlayProjection.fromLatLngToDivPixel(
-                        this.bounds.getNorthEast()
-                    )!;
-
-                    // Resize the image's div to fit the indicated dimensions.
-                    if (this.div) {
-                        this.div.style.left = sw.x + "px";
-                        this.div.style.top = ne.y + "px";
-                        this.div.style.width = ne.x - sw.x + "px";
-                        this.div.style.height = sw.y - ne.y + "px";
-                    }
-                }
-
-                /**
-                 * The onRemove() method will be called automatically from the API if
-                 * we ever set the overlay's map property to 'null'.
-                 */
-                onRemove() {
-                    if (this.div) {
-                        (this.div.parentNode as HTMLElement).removeChild(this.div);
-                        delete this.div;
-                    }
-                }
-
-                /**
-                 *  Set the visibility to 'hidden' or 'visible'.
-                 */
-                hide() {
-                    if (this.div) {
-                        this.div.style.visibility = "hidden";
-                    }
-                }
-
-                show() {
-                    if (this.div) {
-                        this.div.style.visibility = "visible";
-                    }
-                }
-
-                toggle() {
-                    if (this.div) {
-                        if (this.div.style.visibility === "hidden") {
-                            this.show();
-                        } else {
-                            this.hide();
-                        }
-                    }
-                }
-
-                toggleDOM(map: google.maps.Map) {
-                    if (this.getMap()) {
-                        this.setMap(null);
-                    } else {
-                        this.setMap(map);
-                    }
-                }
-            }
-
             if (!mapRef.current || !window.google) return;
 
-            const bounds = new google.maps.LatLngBounds(
-                new google.maps.LatLng(62.281819, -150.287132),
-                new google.maps.LatLng(62.400471, -150.005608)
-            );
-
-            // The photograph is courtesy of the U.S. Geological Survey.
-            let image = "https://developers.google.com/maps/documentation/javascript/";
-
-            image += "examples/full/images/talkeetna.png";
 
             const map = new window.google.maps.Map(mapRef.current, {
                 mapTypeControl: false,
                 center: { lat: 42.32610824896946, lng: -71.14955534500426},
-                zoom: 19,
+                zoom: 23,
             });
 
             const flightPlanCoordinates = [
@@ -169,32 +66,21 @@ const GGMap: React.FC = () => {
                 strokeWeight: 2,
             });
 
-            const overlay: USGSOverlay = new USGSOverlay(bounds, image);
+            const imageBounds = {
+                north: 42.32629501962394,
+                south: 42.32567963129395,
+                east: -71.14924542914931,
+                west: -71.15010356316003,
+            };
 
-
-            const toggleButton = document.createElement("button");
-
-            toggleButton.textContent = "Toggle";
-            toggleButton.classList.add("custom-map-control-button");
-
-            const toggleDOMButton = document.createElement("button");
-
-            toggleDOMButton.textContent = "Toggle DOM Attachment";
-            toggleDOMButton.classList.add("custom-map-control-button");
-
-            toggleButton.addEventListener("click", () => {
-                overlay.toggle();
-            });
-
-            toggleDOMButton.addEventListener("click", () => {
-                overlay.toggleDOM(map);
-            });
-
-            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleDOMButton);
-            map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
+            historicalOverlay = new google.maps.GroundOverlay(
+                floor_1_map,
+                imageBounds
+            );
 
             new AutocompleteDirectionsHandler(map);
-            overlay.setMap(map);
+            console.log('hi');
+            historicalOverlay.setMap(map);
             flightPath.setMap(map);
 
         };
@@ -208,23 +94,6 @@ const GGMap: React.FC = () => {
     return (
         <div>
             <div style={{ display: 'flex', flexDirection: 'column' }}>
-                {/*<input id="origin-input" type="text" placeholder="Origin" />*/}
-                {/*<input id="destination-input" type="text" placeholder="Destination" />*/}
-
-                {/*This currently is not working because we have yet to implement the path to any of the option*/}
-
-
-                {/*<select id="mode-selector">*/}
-                {/*    <option value="WALKING" id="changemode-walking">*/}
-                {/*        Walking*/}
-                {/*    </option>*/}
-                {/*    <option value="TRANSIT" id="changemode-transit">*/}
-                {/*        Transit*/}
-                {/*    </option>*/}
-                {/*    <option value="DRIVING" id="changemode-driving">*/}
-                {/*        Driving*/}
-                {/*    </option>*/}
-                {/*</select>*/}
             </div>
 
             <div
@@ -237,14 +106,6 @@ const GGMap: React.FC = () => {
 };
 
 export default GGMap;
-
-// Declare window type extension
-declare global {
-    interface Window {
-        initMap: () => void;
-        google: typeof google;
-    }
-}
 
 
 
