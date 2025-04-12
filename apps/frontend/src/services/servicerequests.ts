8e74c7932b147db9f8df21099b4639b984561d99
christinenngo <christinengo05@gmail.com>
2025-04-12 16:18:12 -0400

    fixing database calls still broken

--- apps/frontend/src/services/servicerequests.ts
+++ apps/frontend/src/services/servicerequests.ts
@@ -4,7 +4,7 @@ import { API_ROUTES } from '../../../../packages/common/src/constants.ts';
 import type {ServiceRequest} from 'src/routes/AllServiceRequests.tsx';
 
 async function getAllRequests (): Promise<ServiceRequest> {
-    const services = (await axios.get('/api/servicereqs'));
+    const services = (await axios.get('/api/servicereqs/translator'));
     return services.data;
 }
 
