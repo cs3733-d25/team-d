a6de03a35f13f4c0ebd836059f6988e6933a02fb
christinenngo <christinengo05@gmail.com>
2025-04-12 17:24:30 -0400

    added table for equipment requests

--- apps/frontend/src/routes/AllServiceRequests.tsx
+++ apps/frontend/src/routes/AllServiceRequests.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import getAllRequests from "@/services/servicerequests.ts";
+
 import {
     Table,
     TableBody,
@@ -11,8 +11,7 @@ import {
     TableRow,
 } from "../components/ui/table.tsx";
 import axios from "axios";
-
-interface TranslatorRequest {
+export interface TranslatorRequest {
     languageFrom: string;
     languageTo: string;
     roomNum: number;
@@ -21,15 +20,15 @@ interface TranslatorRequest {
     serviceRequestId: number;
 }
 
-// interface EquipmentRequest {
-//     medicalDevice: string;
-//     signature: string;
-//     quantity: number;
-//     comments: string;
-//     roomNum: string;
-//     startDateTime: string;
-//     endDateTime: string;
-// }
+export interface EquipmentRequest {
+    medicalDevice: string;
+    signature: string;
+    quantity: number;
+    comments: string;
+    roomNum: string;
+    startDateTime: string;
+    endDateTime: string;
+}
 
 export interface ServiceRequest {
     requestId: number;
@@ -37,6 +36,7 @@ export interface ServiceRequest {
     updatedAt: number;
     assignedEmployeeId: number;
     translatorRequest: TranslatorRequest;
+    equipmentRequest: EquipmentRequest;
     requestStatus: string;
     priority: string;
     employeeRequestedById: number;
@@ -44,14 +44,28 @@ export interface ServiceRequest {
 }
 
 export default function ShowAllRequests() {
-    const [data, setData] = useState<ServiceRequest[]>([]);
-
+    const [dataTranslator, setDataTranslator] = useState<ServiceRequest[]>([]);
+    const [dataEquipment, setDataEquipment] = useState<ServiceRequest[]>([]);
 
     useEffect(() => {
         console.log('Fetching---');
         // getRequests();
-        axios.get('api/servicereqs').then((response) => {
-            setData(response.data);
+        axios.get('api/servicereqs/translator').then((response) => {
+            setDataTranslator(response.data);
+            console.log(response.data);
+        })
+
+        axios.get('api/servicereqs/equipment').then((response) => {
+            setDataEquipment(response.data);
+            console.log(response.data);
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log('Fetching---');
+        // getRequests();
+        axios.get('api/servicereqs/equipment').then((response) => {
+            setDataEquipment(response.data);
             console.log(response.data);
         })
     }, [])
@@ -60,34 +74,84 @@ export default function ShowAllRequests() {
         <>
             <div className="min-h-screen w-full p-6 bg-white">
                 <div className="flex items-center gap-4 mb-6">
-                    <h2 className="text-xl font-bold">Service Request Database: Translator Requests</h2>
+                    <h2 className="text-xl font-bold">Service Request Database:</h2>
+                    <br />
                 </div>
 
-                 {/*Data table (headers only for now) */}
+                <h2 className="text-xl font-bold">Translator Requests</h2>
                 <Table>
                     <TableHeader>
                         <TableRow>
                             <TableHead className="w-32">Request ID</TableHead>
+                            <TableHead>Requested By</TableHead>
+                            <TableHead>Department</TableHead>
+                            <TableHead>Assigned Employee</TableHead>
                             <TableHead>Language To</TableHead>
                             <TableHead>Language From</TableHead>
                             <TableHead>Room Number</TableHead>
+                            <TableHead>Priority</TableHead>
+                            <TableHead>Status</TableHead>
                             <TableHead>Created At</TableHead>
                             <TableHead>Updated At</TableHead>
-                            <TableHead>Assigned Employee</TableHead>
                         </TableRow>
                     </TableHeader>
 
-                {/*    /!* Empty table body for now *!/*/}
                     <TableBody>
-                        {data.map((element, i) => (
+                        {dataTranslator.map((element, i) => (
                             <TableRow key={i}>
                                 <TableCell>{element.requestId}</TableCell>
+                                <TableCell>{element.employeeRequestedById}</TableCell>
+                                <TableCell>{element.departmentUnderId}</TableCell>
+                                <TableCell>{element.assignedEmployeeId}</TableCell>
                                 <TableCell>{element.translatorRequest.languageTo}</TableCell>
                                 <TableCell>{element.translatorRequest.languageFrom}</TableCell>
                                 <TableCell>{element.translatorRequest.roomNum}</TableCell>
+                                <TableCell>{element.priority}</TableCell>
+                                <TableCell>{element.requestStatus}</TableCell>
                                 <TableCell>{element.createdAt}</TableCell>
                                 <TableCell>{element.updatedAt}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+
+
+                <h2 className="text-xl font-bold">Equipment Requests</h2>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-32">Request ID</TableHead>
+                            <TableHead>Requested By</TableHead>
+                            <TableHead>Department</TableHead>
+                            <TableHead>Assigned Employee</TableHead>
+                            <TableHead>Medical Device</TableHead>
+                            <TableHead>Quantity</TableHead>
+                            <TableHead>Room Number</TableHead>
+                            <TableHead>Priority</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Comments</TableHead>
+                            <TableHead>Signature</TableHead>
+                            <TableHead>Created At</TableHead>
+                            <TableHead>Updated At</TableHead>
+                        </TableRow>
+                    </TableHeader>
+
+                    <TableBody>
+                        {dataEquipment.map((element, j) => (
+                            <TableRow key={j}>
+                                <TableCell>{element.requestId}</TableCell>
+                                <TableCell>{element.employeeRequestedById}</TableCell>
+                                <TableCell>{element.departmentUnderId}</TableCell>
                                 <TableCell>{element.assignedEmployeeId}</TableCell>
+                                <TableCell>{element.equipmentRequest.medicalDevice}</TableCell>
+                                <TableCell>{element.equipmentRequest.quantity}</TableCell>
+                                <TableCell>{element.equipmentRequest.roomNum}</TableCell>
+                                <TableCell>{element.priority}</TableCell>
+                                <TableCell>{element.requestStatus}</TableCell>
+                                <TableCell>{element.equipmentRequest.comments}</TableCell>
+                                <TableCell>{element.equipmentRequest.signature}</TableCell>
+                                <TableCell>{element.createdAt}</TableCell>
+                                <TableCell>{element.updatedAt}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
