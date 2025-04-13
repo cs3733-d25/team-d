aadb09f0a86a77cfa24d3deb96326e89c2308a4b
avoforme <hophuongthanh028@gmail.com>
2025-04-13 18:06:11 -0400

    merge all as of rn

--- apps/frontend/src/routes/Home.tsx
+++ apps/frontend/src/routes/Home.tsx
@@ -38,9 +38,10 @@ export default function Home() {
                     </div>
                 </div>
 
+
+            </div>
             <div className="place">
                 <Footer />
-            </div>
             </div>
         </div>
     );
