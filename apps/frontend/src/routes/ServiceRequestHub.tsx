3c846e49dffda84580c6792ad7e52364e743aa1f
lucienx626@gmail.com <lucienx626@gmail.com>
2025-04-13 16:39:55 -0400

    Added an overlay

--- apps/frontend/src/routes/ServiceRequestHub.tsx
+++ apps/frontend/src/routes/ServiceRequestHub.tsx
@@ -5,9 +5,16 @@ import ServiceHubBackground from "../public/ServiceHubBackground.png";
 const ServiceRequestHub = () => {
     return (
         <>
+
+
             <img src={ServiceHubBackground} className="absolute h-screen w-screen z-0" />
+
+            {/*Overlay*/}
+            <div className="absolute inset-0 bg-yellow-600/10 mix-blend-multiply pointer-events-none"/>
+            <div className="absolute inset-0 bg-zinc-900/50 pointer-events-none"/>
+
             <div className="relative flex justify-center">
-                <div className="absolute flex items-center justify-center mt-24 bg-gray-300/80 h-20 w-110 rounded-full text-5xl text-bold font-nunito">Service Requests</div>
+                <div className="absolute flex items-center justify-center mt-24 bg-gray-300/60 h-20 w-110 rounded-full text-5xl font-bold font-nunito font-nunito">Service Requests</div>
             </div>
             <div className="relative flex justify-center h-screen w-screen">
                 <div className="absolute mt-80 flex flex-row items-center justify-evenly bg-blue-900/80 w-300 h-100 z-10">
