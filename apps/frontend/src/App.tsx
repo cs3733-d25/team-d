dad1f6cafa460323d4abb38bcd5182d379287c9f
jabo23 <jacob8086b@gmail.com>
2025-04-14 15:12:49 -0400

    questioning everything leads to success

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,16 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "@/components/Navbar";
 import Home from "@/routes/Home";
-import Map from "@/routes/Map";
-import Directory from "@/routes/Directory";
-import KioskDirections from "@/routes/KioskDirections";
-import WithinHospital from "@/routes/WithinHospital";
-import ToHospital from "@/routes/ToHospital";
 import AdminDatabase from "@/routes/AdminDatabase";
 import ServiceRequestHub from "@/routes/ServiceRequestHub.tsx";
 import AllServiceRequests from "@/routes/AllServiceRequests.tsx";
-import GoogleMap from "@/components/map/GoogleMap.tsx";
-import GGMap from "@/GoogleMap/GoogleMap.tsx";
 import Directions from "@/routes/Directions.tsx";
 import Auth0Profile from "@/components/Auth0Profile.tsx";
 
@@ -26,10 +19,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Navbar />}>
                         <Route index element={<Home />} />
-                        <Route path="map" element={<Map />} />
-                        <Route path="directory" element={<Directory />} />
-                        <Route path="within-hospital" element={<WithinHospital />} />
-                        <Route path="to-hospital" element={<ToHospital />} />
+                        <Route path="directory" element={<Directions />} />
                         <Route path="servicerequesthub" element={<ServiceRequestHub />} />
 
                         {/* Protected routes wrapped in ProtectedRoute */}
