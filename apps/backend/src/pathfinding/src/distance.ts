5893d465511aa2f4fa0bd8db1ea92814a4b4fc2f
jabo23 <jacob8086b@gmail.com>
2025-04-15 13:26:27 -0400

    make things happy

--- apps/backend/src/pathfinding/src/distance.ts
+++ apps/backend/src/pathfinding/src/distance.ts
@@ -3,7 +3,7 @@ import type { Coordinates } from 'common/src/constants';
 //Calculates Euclidean distance between two coordinates.
 
 export function euclideanDistance(a: Coordinates, b: Coordinates): number {
-    const dx = b.x - a.x;
-    const dy = b.y - a.y;
+    const dx = b.lat - a.lat;
+    const dy = b.lng - a.lng;
     return Math.sqrt(dx * dx + dy * dy);
 }
