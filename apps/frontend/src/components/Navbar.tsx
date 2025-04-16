5e376b79c98ccdfb58af8296c2f997e398c4c041
lucienx626@gmail.com <lucienx626@gmail.com>
2025-04-16 18:27:08 -0400

    Made the buttons on the nav bar all the same font size

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -37,13 +37,13 @@ export default function Navbar() {
 
 
                             <NavigationMenuItem>
-                                <NavigationMenuLink className={'font-nunito hover:bg-[rgba(0,31,63,0.8)] hover:text-white'}>
+                                <NavigationMenuLink className={'text-base hover:bg-[rgba(0,31,63,0.8)] hover:text-white'}>
                                     <Link to={`/directory`}>Directions</Link>
                                 </NavigationMenuLink>
                             </NavigationMenuItem>
 
                             <NavigationMenuItem>
-                                <NavigationMenuLink className={'font-nunito hover:bg-[rgba(0,31,63,0.8)] hover:text-white'}>
+                                <NavigationMenuLink className={'text-base hover:bg-[rgba(0,31,63,0.8)] hover:text-white'}>
                                     <Link to={`/servicerequesthub`}>Request Service</Link>
                                 </NavigationMenuLink>
                             </NavigationMenuItem>
