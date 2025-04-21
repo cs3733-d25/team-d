272a57c53e99aa6a733d3a1747cf11229f4bae13
keethu-jay <keerthana.sa.jayamoorthy@gmail.com>
2025-04-21 17:06:53 -0400

    made home page like the figma

--- apps/frontend/src/components/ServiceRequest/ServiceReqCard.tsx
+++ apps/frontend/src/components/ServiceRequest/ServiceReqCard.tsx
@@ -3,12 +3,11 @@ import { Dialog, DialogTrigger, DialogContent } from "@/components/ui/dialog.tsx
 
 type ServiceRequestCardProps = {
     title: string;
-    description: string;
     image: any;
     popupComponent: React.ReactNode;
 };
 
-const ServiceRequestCard: React.FC<ServiceRequestCardProps> = ({ title, description, image, popupComponent }) => {
+const ServiceRequestCard: React.FC<ServiceRequestCardProps> = ({ title, image, popupComponent }) => {
     return (
         <Dialog>
             <DialogTrigger asChild>
@@ -17,13 +16,17 @@ const ServiceRequestCard: React.FC<ServiceRequestCardProps> = ({ title, descript
                     <img
                         src={image}
                         alt={title}
-                        className="absolute inset-0 w-full h-full object-contain"
+                        className="absolute inset-0 w-full h-[350px] rounded-md  object-cover "
                     />
 
                     {/* Overlay Text Box */}
-                    <div className="absolute bottom-[50px] left-1/2 transform -translate-x-1/2 w-[250px] bg-white bg-opacity-90 p-4">
-                        <h2 className="text-xl font-bold text-center font-nunito">{title}</h2>
-                        <p className="text-sm text-center text-gray-600">{description}</p>
+                    <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[250px] bg-white rounded-md bg-opacity-90 p-4">
+                        <h2 className="text-3xl font-bold text-center font-nunito mb-2">{title}</h2>
+                        <button
+                            className=" w-full px-4 py-2 bg-[#012D5A] text-white text-lg rounded-md  hover:bg-blue-600 active:scale-95 focus:outline-none"
+                        >
+                            Submit a Form
+                        </button>
                     </div>
                 </div>
             </DialogTrigger>
--- apps/frontend/src/routes/ServiceRequestHub.tsx
+++ apps/frontend/src/routes/ServiceRequestHub.tsx
@@ -4,10 +4,10 @@ import {faLanguage} from "@fortawesome/free-solid-svg-icons";
 import {faHandHoldingDroplet} from "@fortawesome/free-solid-svg-icons";
 import {faScrewdriverWrench} from "@fortawesome/free-solid-svg-icons";
 import {faShield} from "@fortawesome/free-solid-svg-icons";
-import translatorImage from "../public/translatorReq.jpg"
-import sanitationImage from "../public/translatorReq.jpg"
-import equipmentImage from "../public/translatorReq.jpg"
-import securityImage from "../public/translatorReq.jpg"
+import translatorImage from "../public/translator_pic.jpg"
+import sanitationImage from "../public/sanitation_pic.webp"
+import equipmentImage from "../public/medical_pic.jpg"
+import securityImage from "../public/security_pic.jpg"
 import TranslatorRequestPopup from "@/components/ServiceRequest/TranslatorRequest/TranslatorRequestPopup.tsx";
 import SanitationPopup  from "@/components/ServiceRequest/SanitationRequest/SanitationPopup.tsx";
 import EquipmentRequestPopup from "@/components/ServiceRequest/EquipmentRequest/EquipmentRequestPopup.tsx";
@@ -23,42 +23,46 @@ import ServiceHubBackground from "../public/ServiceHubBackground.png";
 
 const ServiceRequestHub = () => {
     return (
-        <div className={'bg-gradient-to-b from-[#012D5A] via-[#ADDDE5] to-[#012D5A]'}>
-            <div className="relative flex justify-center">
-                <div className="relative z-10 flex items-center justify-center mt-12 bg-gray-300/60 h-20 w-110 text-5xl font-bold font-nunito">
+        <div className="min-h-screen flex flex-col bg-[url(../public/service_request_bg.jpg)] bg-cover bg-center bg-no-repeat">
+            {/* Overlay for blending */}
+
+            {/* Main Content */}
+            <main className="relative flex flex-col items-center justify-center z-10 flex-grow">
+                <div className="absolute inset-0 bg-yellow-600/10 mix-blend-multiply pointer-events-none"></div>
+                <div className="absolute inset-0 bg-zinc-900/50 pointer-events-none"></div>
+                <div className="relative text-5xl z-10 mb-10 pb-4 flex top-x items-center justify-center justify-top mt-5 h-20 w-110 text-5xl font-bold font-nunito">
                     Service Requests
                 </div>
-            </div>
-            <div className="relative flex items-center justify-center h-screen w-screen overflow-hidden">
-                <div className="flex flex-row items-center justify-evenly w-300 h-100 rounded-md gap-4">
-                    <ServiceRequestCard
-                        title="Translator Request"
-                        description="Request assistance with translation services."
-                        image={translatorImage}
-                        popupComponent={<TranslatorServiceRequest />}
-                    />
-                    <ServiceRequestCard
-                        title="Sanitation Request"
-                        description="Request assistance with translation services."
-                        image={sanitationImage}
-                        popupComponent={<SanitationServiceRequest />}
-                    />
-                    <ServiceRequestCard
-                        title="Equipment Request"
-                        description="Request assistance with translation services."
-                        image={equipmentImage}
-                        popupComponent={<EquipmentServiceRequest/>}
-                    />
-                    <ServiceRequestCard
-                        title="Security Request"
-                        description="Request assistance with translation services."
-                        image={securityImage}
-                        popupComponent={<SecurityServiceRequest />}
-                    />
+                <div className="relative flex items-center justify-center h-auto w-full overflow-hidden p-8">
+                    <div className="flex justify-between items-center w-full px-8">
+                        <ServiceRequestCard
+                            title="Translator"
+                            image={translatorImage}
+                            popupComponent={<TranslatorServiceRequest />}
+                        />
+                        <ServiceRequestCard
+                            title="Sanitation"
+                            image={sanitationImage}
+                            popupComponent={<SanitationServiceRequest />}
+                        />
+                        <ServiceRequestCard
+                            title="Equipment"
+                            image={equipmentImage}
+                            popupComponent={<EquipmentServiceRequest />}
+                        />
+                        <ServiceRequestCard
+                            title="Security"
+                            image={securityImage}
+                            popupComponent={<SecurityServiceRequest />}
+                        />
+                    </div>
+
                 </div>
-            </div>
+            </main>
+
         </div>
     );
-}
+};
+
 
 export default ServiceRequestHub;
