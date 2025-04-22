9d4b8c387ec48a20fdd3dbd873ac8d81b6ec69dd
christinenngo <christinengo05@gmail.com>
2025-04-22 17:33:44 -0400

    made get and put requests for new tables (graph, floorGraph, parkingGraph, building)

--- apps/backend/src/routes/directory.ts
+++ apps/backend/src/routes/directory.ts
@@ -1,8 +1,10 @@
 // import express, { Router, Request, Response } from 'express';
 // import PrismaClient from '../bin/prisma-client';
 // import { Prisma } from 'database';
+// import {EditorParkingGraph} from "common/src/constants.ts";
 // const router: Router = express.Router();
 //
+// // GET request for all hospitals
 // router.get('/', async (req, res) => {
 //     const data = await PrismaClient.hospital.findMany({
 //         include: {
@@ -25,7 +27,7 @@
 //     }
 // });
 //
-// // Returns all departments in the directory, if any
+// // GET request for all departments in a specific hospital
 // router.get('/:id/all', async function (req: Request, res: Response) {
 //     const hospitalId: number = Number(req.params.id);
 //     const departments = await PrismaClient.department.findMany({
@@ -41,7 +43,7 @@
 //     }
 // });
 //
-// // Returns all departments in the directory, if any
+// // GET request for all departments in the directory, if any
 // router.get('/all', async function (req: Request, res: Response) {
 //     const departments = await PrismaClient.department.findMany();
 //     // if there are no departments found
@@ -54,7 +56,7 @@
 //     }
 // });
 //
-// // post request to add departments to the database
+// // POST request to add departments to the database
 // router.post('/', async function (req: Request, res: Response) {
 //     const departmentDataAttempt: Prisma.DepartmentCreateInput = req.body;
 //     try {
@@ -68,7 +70,8 @@
 //
 //     res.sendStatus(200);
 // });
-// // delete all departments in the database
+//
+// // DELETE all departments in the database
 // router.delete('/', async function (req: Request, res: Response) {
 //     try {
 //         await PrismaClient.department.deleteMany({});
@@ -78,6 +81,75 @@
 //         return;
 //     }
 //     res.sendStatus(200);
+// });
+//
+// // GET request for all buildings
+// router.get('/directory/buildings', async (req: Request, res: Response) => {
+//     const buildings = await PrismaClient.building.findMany({
+//         include: {
+//             Hospital: {},
+//             FloorGraphs: {},
+//         },
+//     });
+//     if (buildings == null) {
+//         console.error('No buildings found in database');
+//         res.sendStatus(204);
+//     } else {
+//         console.log(buildings);
+//         res.json(buildings);
+//     }
+// });
+//
+// // PUT request for building with specific Id
+// router.put('/directory/buildings/:id', async function (req: Request, res: Response) {
+//     // parse id into variable
+//     const buildingId: number = Number(req.params.id);
+//
+//     // find building with id
+//     const request = await PrismaClient.building.findUnique({
+//         where: { buildingId: buildingId },
+//     });
+//
+//     // error if no building with the id is found
+//     if (request == null) {
+//         console.error(`The building with Id ${buildingId} not found in database!`);
+//         res.status(404);
+//     }
+//     // success: update specified building
+//     else {
+//         try {
+//             const {
+//                 name,
+//                 address,
+//                 hospitalId,
+//                 Hospital,
+//                 FloorGraphs,
+//             } = req.body;
+//             const [
+//                 updateBuilding,
+//             ] = await PrismaClient.$transaction([
+//                 PrismaClient.building.update({
+//                     where: { buildingId: buildingId },
+//                     data: {
+//                         name,
+//                         address,
+//                         hospitalId,
+//                         Hospital,
+//                         FloorGraphs,
+//                     },
+//                 }),
+//             ]);
+//             // send 200 and updated building if success
+//             res.status(200).json({
+//                 message: 'Successfully updated building',
+//                 updateBuilding,
+//             });
+//             // send 400 and error message if building cannot be updated
+//         } catch (error) {
+//             console.error(`Unable to update building ${buildingId}: ${error}`);
+//             res.sendStatus(400);
+//         }
+//     }
 // });
 //
 // export default router;
--- apps/backend/src/routes/editor.ts
+++ apps/backend/src/routes/editor.ts
@@ -2,9 +2,10 @@ import express, { Router, Request, Response } from 'express';
 const router: Router = express.Router();
 
 import PrismaClient from '../bin/prisma-client';
-import { EditorGraph } from 'common/src/constants.ts';
+import {EditorFloorGraph, EditorParkingGraph, EditorGraph} from 'common/src/constants.ts';
 
-router.get('/', async (req: Request, res: Response) => {
+// GET request for all graphs
+router.get('/editor/graphs', async (req: Request, res: Response) => {
     const data = await PrismaClient.graph.findMany({
         include: {
             Nodes: {},
@@ -13,8 +14,207 @@ router.get('/', async (req: Request, res: Response) => {
             ParkingGraph: {},
         },
     });
+    if (data == null) {
+        console.error('No graphs found in database');
+        res.sendStatus(204);
+    } else {
+        console.log(data);
+        res.json(data as EditorGraph[]);
+    }
+});
 
-    res.json(data as EditorGraph[]);
+// GET request for all floor graphs
+router.get('/editor/floor', async (req: Request, res: Response) => {
+    const data = await PrismaClient.floorGraph.findMany({
+        include: {
+            Building: {},
+            Graph: {},
+        },
+    });
+    if (data == null) {
+        console.error('No floor graphs found in database');
+        res.sendStatus(204);
+    } else {
+        console.log(data);
+        res.json(data as EditorFloorGraph[]);
+    }
+});
+
+// GET request for all parking graphs
+router.get('/editor/parking', async (req: Request, res: Response) => {
+    const data = await PrismaClient.parkingGraph.findMany({
+        include: {
+            Hospital: {},
+            Graph: {},
+        },
+    });
+    if (data == null) {
+        console.error('No parking graphs found in database');
+        res.sendStatus(204);
+    } else {
+        console.log(data);
+        res.json(data as EditorParkingGraph[]);
+    }
+});
+
+// PUT request for graph with specific Id
+router.put('/editor/graphs/:id', async function (req: Request, res: Response) {
+    // parse id into variable
+    const graphId: number = Number(req.params.id);
+
+    // find graph with id
+    const request = await PrismaClient.graph.findUnique({
+        where: { graphId: graphId },
+    });
+
+    // error if no graph with the id is found
+    if (request == null) {
+        console.error(`The graph with Id ${graphId} not found in database!`);
+        res.status(404);
+    }
+    // success: update specified graph
+    else {
+        try {
+            const {
+                graphType,
+                FloorGraph,
+                ParkingGraph,
+                Nodes,
+                Edges,
+            } = req.body;
+            const [
+                updateGraph,
+            ] = await PrismaClient.$transaction([
+                PrismaClient.graph.update({
+                    where: { graphId: graphId },
+                    data: {
+                        graphType,
+                        FloorGraph,
+                        ParkingGraph,
+                        Nodes,
+                        Edges,
+                    },
+                }),
+            ]);
+            // send 200 and updated graph if success
+            res.status(200).json({
+                message: 'Successfully updated graph',
+                updateGraph,
+            });
+            // send 400 and error message if graph be updated
+        } catch (error) {
+            console.error(`Unable to update graph ${graphId}: ${error}`);
+            res.sendStatus(400);
+        }
+    }
+});
+
+// PUT request for floor graph with specific Id
+router.put('/editor/floor/:id', async function (req: Request, res: Response) {
+    // parse id into variable
+    const graphId: number = Number(req.params.id);
+
+    // find floor graph with id
+    const request = await PrismaClient.floorGraph.findUnique({
+        where: { graphId: graphId },
+    });
+
+    // error if no floor graph with the id is found
+    if (request == null) {
+        console.error(`The floor graph with Id ${graphId} not found in database!`);
+        res.status(404);
+    }
+    // success: update specified floor graph
+    else {
+        try {
+            const {
+                floorNum,
+                image,
+                imageBoundsNorth,
+                imageBoundsSouth,
+                imageBoundsEast,
+                imageBoundsWest,
+                buildingId,
+                Building,
+                Departments,
+            } = req.body;
+            const [
+                updateGraph,
+            ] = await PrismaClient.$transaction([
+                PrismaClient.floorGraph.update({
+                    where: { graphId: graphId },
+                    data: {
+                        floorNum,
+                        image,
+                        imageBoundsNorth,
+                        imageBoundsSouth,
+                        imageBoundsEast,
+                        imageBoundsWest,
+                        buildingId,
+                        Building,
+                        Departments,
+                    },
+                }),
+            ]);
+            // send 200 and updated floor graph if success
+            res.status(200).json({
+                message: 'Successfully updated floor graph',
+                updateGraph,
+            });
+            // send 400 and error message if floor graph cannot be updated
+        } catch (error) {
+            console.error(`Unable to update floor graph ${graphId}: ${error}`);
+            res.sendStatus(400);
+        }
+    }
+});
+
+// PUT request for parking graph with specific Id
+router.put('/editor/parking/:id', async function (req: Request, res: Response) {
+    // parse id into variable
+    const graphId: number = Number(req.params.id);
+
+    // find parking graph with id
+    const request = await PrismaClient.parkingGraph.findUnique({
+        where: { graphId: graphId },
+    });
+
+    // error if no parking graph with the id is found
+    if (request == null) {
+        console.error(`The parking graph with Id ${graphId} not found in database!`);
+        res.status(404);
+    }
+    // success: update specified parking graph
+    else {
+        try {
+            const {
+                Graph,
+                Hospital,
+                hospitalId,
+            } = req.body;
+            const [
+                updateGraph,
+            ] = await PrismaClient.$transaction([
+                PrismaClient.parkingGraph.update({
+                    where: { graphId: graphId },
+                    data: {
+                        Graph,
+                        Hospital,
+                        hospitalId,
+                    },
+                }),
+            ]);
+            // send 200 and updated parking graph if success
+            res.status(200).json({
+                message: 'Successfully updated parking graph',
+                updateGraph,
+            });
+            // send 400 and error message if parking graph cannot be updated
+        } catch (error) {
+            console.error(`Unable to update parking graph ${graphId}: ${error}`);
+            res.sendStatus(400);
+        }
+    }
 });
 
 export default router;
