46cde4d75fef9b4e14ccf91d14a175bfca4d6080
himynameismaggie <mghosie@wpi.edu>
2025-04-23 13:46:50 -0400

    merged with map stuff(enhancement/google-maps-refactored)

--- apps/frontend/src/components/MapButton.tsx
+++ apps/frontend/src/components/MapButton.tsx
@@ -8,7 +8,7 @@ const MapButton: React.FC = () => {
     return (
         <div className="">
             <button onClick={redirectToMaps}
-                    className="text-2xl w-80 border-4 border-white rounded-full text-center bg-black/30 hover:bg-black/60 text-white
+                    className="w-full border-4 border-white rounded-b-lg text-center bg-black/30 hover:bg-black/60 text-white
                     animate-in fade-in zoom-in duration-500 p-4 font-nunito hover:scale-110 cursor-pointer"
             >
                 DIRECTIONS
