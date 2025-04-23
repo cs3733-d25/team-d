fc38a2ff731d9e3cd6b442239a96d12db2088ae3
BramRBN2004 <smallb869@gmail.com>
2025-04-22 21:48:19 -0400

    merge

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFacebook, faInstagram, faLinkedin, faSquareXTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
 
-
+//
 // Use for comments
 {/**/}
 export default function Footer() {
