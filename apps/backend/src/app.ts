80ab751f8f18b64ac9b8eb625804a295a96ed8cc
avoforme <hophuongthanh028@gmail.com>
2025-04-25 15:58:02 -0400

    change routes and add comments

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -30,10 +30,15 @@ app.use(express.json()); // This processes requests as JSON
 app.use(express.urlencoded({ extended: false })); // URL parser
 app.use(cookieParser()); // Cookie parser
 
+
+/**
+ * All routers here should be accessible for both logIned and non-logedIn users
+ */
 app.use(API_ROUTES.PATHFIND, pathfindRouter);
 app.use(API_ROUTES.DEPARTMENT, directoryRouter);
-
 app.use(API_ROUTES.SERVICEREQS, servicereqsRouter);
+app.use(API_ROUTES.EMPLOYEE, employeeRouter);
+
 
 // If we're not in test mode, enable the auth0 enforcement
 if (!process.env['VITETEST']) {
@@ -48,33 +53,16 @@ if (!process.env['VITETEST']) {
     );
 }
 
+/**
+ * All routers here should be accessible for both ONLY (!!!) logIned users
+ */
 // Setup routers. ALL ROUTERS MUST use /api as a start point, or they
 // won't be reached by the default proxy and prod setup
 app.use(API_ROUTES.HEALTHCHECK, healthcheckRouter);
-app.use(API_ROUTES.EMPLOYEE, employeeRouter);
 app.use(API_ROUTES.ASSIGNED, assignedRouter);
 app.use(API_ROUTES.EDITOR, editorRouter);
 // app.use(API_ROUTES.PATHFINDING, pathfindingRouter);
 
-/**
- * Auth0
- */
-// const config = {
-//     authRequired: false,
-//     auth0Logout: true,
-//     secret: 'yada',
-//     baseURL: 'http://localhost:3000',
-//     clientID: 'oTdQpRiO6NMqpsVbTLhp6Kk5egdnFmEs',
-//     issuerBaseURL: 'https://dev-b5d68fi8od5s513y.us.auth0.com',
-// };
-//
-// // auth router attaches /login, /logout, and /callback routes to the baseURL
-// app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-// app.get('/', (req, res) => {
-//     res.send(req.oidc.sisAuthenticated() ? 'Logged in' : 'Logged out');
-// });
 
 /**
  * Catch all 404 errors, and forward them to the error handler
