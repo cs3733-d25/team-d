9c2c25b7b22a456b3b4bd4cd50c45f6b0531c400
avoforme <hophuongthanh028@gmail.com>
2025-04-25 13:47:58 -0400

    hihi

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -6,13 +6,17 @@ import './styles/styles.css';
 
 
 // Entry point where root component is rendered into the DOM
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Auth0Provider
+        useRefreshTokens={true}
+        cacheLocation="localstorage"
         domain="dev-b5d68fi8od5s513y.us.auth0.com"
         clientId="jepCXrJUcBq34pKdSGGzd2sidUxVpnsL"
         authorizationParams={{
-            redirect_uri: window.location.origin + '/directory'
-            // redirect_uri: 'http://localhost:3000/directory'
+            redirect_uri: window.location.origin + '/directory',
+            scope: 'openid profile email offline_access',
         }}
     >
         <App />
