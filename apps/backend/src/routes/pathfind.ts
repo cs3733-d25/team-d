5ab9c1ffc077f3f1adb2db976376c69947c43614
Jiaming Du (Felix) <felixdu10>
2025-04-27 17:36:09 -0400

    update euclidean to haversine

--- apps/backend/src/routes/pathfind.ts
+++ apps/backend/src/routes/pathfind.ts
@@ -213,16 +213,17 @@ router.get('/path-to-dept/:did', async (req: Request, res: Response) => {
     }
 
     const checkInNodeId = checkInCandidates.reduce((closest, node) => {
-        if (
-            euclideanDistance(
-                { lat: node.lat, lng: node.lng },
-                { lat: department.lat, lng: department.lng }
-            ) <
-            euclideanDistance(
-                { lat: closest.lat, lng: closest.lng },
-                { lat: department.lat, lng: department.lng }
-            )
-        ) {
+        const nodeDistance = haversineDistance(
+            { lat: node.lat, lng: node.lng },
+            { lat: department.lat, lng: department.lng }
+        );
+
+        const closestDistance = haversineDistance(
+            { lat: closest.lat, lng: closest.lng },
+            { lat: department.lat, lng: department.lng }
+        );
+
+        if (nodeDistance < closestDistance) {
             return node;
         } else {
             return closest;
