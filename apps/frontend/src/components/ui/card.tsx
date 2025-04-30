974f04bb0c58c8a5f47198c2f5a1892e99d794d9
himynameismaggie <mghosie@wpi.edu>
2025-04-29 22:01:35 -0400

    Type request breakdown chart

--- apps/frontend/src/components/ui/card.tsx
+++ apps/frontend/src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-4 rounded-xl border shadow-sm py-4 sm:w-40 md:w-48 lg:w-60",
+        "bg-card text-card-foreground flex flex-col gap-4 rounded-xl border shadow-sm py-4",
           className
       )}
       {...props}
