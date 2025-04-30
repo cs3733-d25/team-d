3020c3d276e8944e83a673a7285ffaee4af4af0f
aliramyriad <aliriad056@gmail.com>
2025-04-29 20:57:46 -0400

    Update NewDirections.tsx

--- apps/frontend/src/routes/NewDirections.tsx
+++ apps/frontend/src/routes/NewDirections.tsx
@@ -39,6 +39,8 @@ interface TransportModeOption {
     icon: IconDefinition;
 }
 
+//const primaryDarkBlue = "#012D5A";
+//const primaryOrange = "#D47F00";
 const transportModes: TransportModeOption[] = [
     { value: "DRIVING", label: "Driving", icon: faCar },
     { value: "WALKING", label: "Walking", icon: faWalking },
@@ -47,16 +49,35 @@ const transportModes: TransportModeOption[] = [
 ];
 
 export default function NewDirections() {
+
+    // References for the div that holds
+    // the gmap and the autocomplete input,
+    // needed for compatibility w/ gmap api
     const mapRef = useRef<HTMLDivElement>(null);
     const autocompleteRef = useRef<HTMLInputElement>(null);
 
+    // 'backend' for google map
     const [map, setMap] = useState<PathfindingMap>();
-    const [displayData, setDisplayData] = useState<PathfindingOptions>({ hospitals: [] });
+
+    // all hospitals and their departments
+    const [displayData, setDisplayData] = useState<PathfindingOptions>({
+        hospitals: [],
+    });
+
+    // for storing current selection
     const [selectedHospital, setSelectedHospital] = useState<HospitalOptions | null>(null);
     const [selectedDepartment, setSelectedDepartment] = useState<DepartmentOptions | null>(null);
+
     const [pathfindingResults, setPathfindingResults] = useState<PathfindingResults | null>(null);
+
     const [tts, setTts] = useState<boolean>(false);
+
+    // const [pathfindingResponse, setPathfindingResponse] = useState<PathfindingResponse>();
+    //
+    // const [directionsSteps, setDirectionsSteps] = useState<DirectionsStep[]>([]);
+    // const [pathfindingResponse, setPathfindingResponse] = useState<PathfindingResponse>();
     const [selectedMode, setSelectedMode] = useState<string>("DRIVING");
+
     const [currentStep, setCurrentStep] = useState<number>(-1);
 
     const setPathfindingResultsExternal = (results: PathfindingResults | null) => {
@@ -65,8 +86,9 @@ export default function NewDirections() {
     }
 
     useEffect(() => {
+        console.log('useEffect NewDirections');
         const fetchMap = async () => {
-            if (mapRef.current && autocompleteRef.current) {
+            if (mapRef.current && autocompleteRef.current ) {
                 setMap(await PathfindingMap.makeMap(mapRef.current, autocompleteRef.current, setPathfindingResultsExternal));
             }
         }
@@ -75,6 +97,7 @@ export default function NewDirections() {
                 setDisplayData(response.data as PathfindingOptions);
             });
         });
+
     }, []);
 
     const handleHospitalChange = (value: string) => {
@@ -82,33 +105,55 @@ export default function NewDirections() {
     }
 
     const handleDepartmentChange = (value: string) => {
-        if (!selectedHospital || !map) return;
-        const newDept = selectedHospital.departments.find(d => d.name === value);
-        if (!newDept) return;
-        setSelectedDepartment(newDept);
-        map.setDepartment(newDept);
+        if (!selectedHospital) return;
+
+        const newDepartment = selectedHospital.departments.find(d => d.name === value);
+        if (!newDepartment) return;
+        setSelectedDepartment(newDepartment);
+
+        if (!map) return;
+        map.setDepartment(newDepartment);
+
+        // setCurrentStep(-1);
     }
 
     const handleModeChange = (mode: string) => {
         setSelectedMode(mode);
-        if (map) map.setTravelMode(mode);
+        if (!map) return;
+        map.setTravelMode(mode);
     }
 
     const handleNextStep = () => {
         if (!pathfindingResults || currentStep >= pathfindingResults.directions.length - 1) return;
+
         map?.setCurrentStepIdx(currentStep + 1, tts);
         setCurrentStep(currentStep + 1);
+        console.log(currentStep + 1);
     }
 
     const handlePrevStep = () => {
         if (!pathfindingResults || currentStep < 1) return;
+
         map?.setCurrentStepIdx(currentStep - 1, tts);
         setCurrentStep(currentStep - 1);
+        console.log(currentStep - 1);
+    }
+
+    const handleZoom = () => {
+        // if (!pathfindingResponse || !map) return;
+        // map.recenter(
+        //     pathfindingResponse.parkingLotPath.path[0].lat,
+        //     pathfindingResponse.parkingLotPath.path[0].lng, 20
+        // );
+    }
+
+    const handleStepSelect = () => {
+
     }
 
     const handleRecenter = () => {
         if (map && pathfindingResults) {
-            map.setCurrentStepIdx(currentStep, false);
+            map?.setCurrentStepIdx(currentStep, false);
         }
     };
 
@@ -121,29 +166,30 @@ export default function NewDirections() {
                 <div className="flex flex-row">
                     <Card className="flex-1 grow">
                         <CardContent>
+                            {/*TODO: find a better way of doing this, copied from components/ui/input.tsx*/}
                             <Label className="mb-1">Start Location</Label>
                             <input
                                 ref={autocompleteRef}
                                 id="start-input"
                                 type="text"
+                                data-slot="input"
                                 className={cn(
                                     "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
                                     "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-                                    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-                                    "mb-4"
+                                    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive", "mb-4"
                                 )}
                             />
 
                             <Label className="mb-2">Transport Mode</Label>
-                            <div className="flex flex-col items-center justify-center gap-6 rounded-md shadow-md p-4 bg-[#012D5A] mb-4">
+                            <div className="flex flex-col items-center justify-center gap-6 rounded-md shadow-md p-4 bg-[#012D5A] mb-4"> {/* Rounded box container */}
                                 <div className="flex gap-6">
                                     {transportModes.map((mode) => (
                                         <button
                                             key={mode.value}
                                             onClick={() => handleModeChange(mode.value)}
                                             className={cn(
-                                                "p-2 rounded-md shadow-sm border",
-                                                `text-[#012D5A]`,
+                                                "p-2 rounded-md shadow-sm border", // Added border back
+                                                `text-[#012D5A]`, // Default text color
                                                 selectedMode === mode.value
                                                     ? `bg-[#012D5A] text-[#D47F00] foreground border-[#D47F00]`
                                                     : `bg-[#F1F1F1] text-[#012D5A] hover:bg-gray-100 border-gray-300`
@@ -166,36 +212,42 @@ export default function NewDirections() {
                                 <SelectContent>
                                     <SelectGroup>
                                         <SelectLabel>Hospitals</SelectLabel>
-                                        {displayData.hospitals.map(h => (
-                                            <SelectItem key={h.hospitalId} value={h.name}>{h.name}</SelectItem>
+                                        {displayData.hospitals.map((h: HospitalOptions) => (
+                                            <SelectItem key={h.hospitalId + 1} value={h.name}>
+                                                {h.name}
+                                            </SelectItem>
                                         ))}
                                     </SelectGroup>
                                 </SelectContent>
                             </Select>
 
-                            {selectedHospital && (
+                            {selectedHospital &&
                                 <>
+                                    {/*<Separator className="mt-4 mb-4" />*/}
+
                                     <Label>Department</Label>
                                     <Select onValueChange={handleDepartmentChange}>
                                         <SelectTrigger className="w-full mt-1 mb-4">
                                             <SelectValue placeholder="Choose a department..." />
                                         </SelectTrigger>
                                         <SelectContent>
-                                            <SelectGroup>
+                                            <SelectGroup key="0">
                                                 <SelectLabel>Departments</SelectLabel>
-                                                {selectedHospital.departments.map(d => (
-                                                    <SelectItem key={d.departmentId} value={d.name}>{d.name}</SelectItem>
+                                                {selectedHospital.departments.map((d: DepartmentOptions) => (
+                                                    <SelectItem key={d.departmentId + 1} value={d.name}>
+                                                        {d.name}
+                                                    </SelectItem>
                                                 ))}
                                             </SelectGroup>
                                         </SelectContent>
                                     </Select>
                                 </>
-                            )}
+                            }
                         </CardContent>
                     </Card>
                 </div>
 
-                {pathfindingResults && (
+                {pathfindingResults &&
                     <>
                         <Separator className="mt-4 mb-4" />
                         <div className="flex flex-row">
@@ -207,8 +259,8 @@ export default function NewDirections() {
                                             <Switch className="data-[state=checked]:bg-blue-900" onCheckedChange={setTts} />
                                         </Label>
                                         <Button
-                                            className="flex-1 grow m-2 bg-blue-900"
                                             onClick={handleRecenter}
+                                            className="flex-1 grow m-2 bg-blue-900"
                                         >
                                             <FontAwesomeIcon icon={faCrosshairs} />
                                             Re-center
@@ -223,15 +275,22 @@ export default function NewDirections() {
                                 <CardContent>
                                     <div className="h-[400px] overflow-y-scroll">
                                         {pathfindingResults.directions.map((step, i) => (
-                                            <div key={i} className={`relative group px-2 ${currentStep === i ? 'bg-gray-200 rounded-md' : 'bg-white'}`} onClick={() => { map?.setCurrentStepIdx(i, tts); setCurrentStep(i); }}>
-                                                <FontAwesomeIcon icon={
-                                                    step.icon === 'right' ? faArrowRight :
-                                                        step.icon === 'left' ? faArrowLeft : faArrowUp
-                                                } />
-                                                <span className="text-blue-500"> {step.instructions}</span>
+                                            <div className={`relative group px-2 ${currentStep === i ? 'bg-gray-200 rounded-md' : 'bg-white'}`}
+                                                 onClick={() => {
+                                                     map?.setCurrentStepIdx(i, tts);
+                                                     setCurrentStep(i);
+                                                     console.log(i);
+                                                 }}>
+                                                {step.icon === "right" ? <FontAwesomeIcon icon={faArrowRight}/>
+                                                    : step.icon === "left" ? <FontAwesomeIcon icon={faArrowLeft}/>
+                                                        : <FontAwesomeIcon icon={faArrowUp}/>}
+                                                <span> </span>
+                                                <span className="text-blue-500">{step.instructions}</span>
                                                 <br/>
                                                 <span className="text-gray-500">{step.time} ({step.distance})</span>
-                                                <span className="absolute bottom-0 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-black">Click to view</span>
+                                                <span className="absolute bottom-0 right-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-black ml-30">
+                                                    Click to view
+                                                </span>
                                                 <br/><br/>
                                             </div>
                                         ))}
@@ -240,9 +299,11 @@ export default function NewDirections() {
                             </Card>
                         </div>
                     </>
-                )}
+                }
             </div>
-            <div ref={mapRef} className="flex-3" />
+            <div ref={mapRef} className="flex-3">
+                {/* Google map will go here */}
+            </div>
         </div>
     );
 }
